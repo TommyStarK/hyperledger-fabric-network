21b9369096eb16af19b2d4ee164488dfe4a49683
Thomas Milox <thomas.milox@ibm.com>
2020-08-15 16:44:03 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hyperledger-fabric-network
+
+Hyperledger Fabric network for demo purposes only
+
+## Usage
+
+```bash
+❯ ./generate-artifacts.sh
+
+❯ docker-compose up -d
+```
---
+++ generate-artifacts.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Exit on first error, print all commands.
+set -e
+rm -rf channel-artifacts
+rm -rf crypto-config
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; cryptogen generate --config=./crypto-config.yaml"
+
+mkdir channel-artifacts
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile OrdererGenesis -channelID orderer-system-channel -outputBlock ./channel-artifacts/genesis.block"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile Channel12 -outputCreateChannelTx ./channel-artifacts/channel12.tx -channelID channel12"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile ChannelAll -outputCreateChannelTx ./channel-artifacts/channelall.tx -channelID channelall"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile Channel12 -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors_channel12.tx -channelID channel12 -asOrg Org1MSP"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile Channel12 -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors_channel12.tx -channelID channel12 -asOrg Org2MSP"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile ChannelAll -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors_channelall.tx -channelID channelall -asOrg Org1MSP"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile ChannelAll -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors_channelall.tx -channelID channelall -asOrg Org2MSP"
+
+docker run -it --rm --mount type=bind,src=`pwd`,dst=/crypto hyperledger/fabric-tools:amd64-2.2.0 bash -c \
+"cd /crypto ; export FABRIC_CFG_PATH=/crypto ; configtxgen -profile ChannelAll -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors_channelall.tx -channelID channelall -asOrg Org3MSP"
